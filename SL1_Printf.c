643bd9df896699d6ac09338ce71208e020c47653
VuVanHuy <huyvv10@fpt.edu.vn>
2026-09-07 12:15:16 +0700

    Initial commit

---
+++ SL1_Printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+//	printf("I'm Huy. I'm a \"FPT University teacher\""); 
+//	printf("\nC:\\PRF192\\Workshop1\\Q1.c");
+
+	printf("I'm Huy. I'm a \"FPT University teacher\"\nC:\\PRF192\\Workshop1\\Q1.c");
+	return 0;
+}
