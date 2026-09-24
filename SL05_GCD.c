00fede94a409bf28c3a753c50a9e7d4c2066158f
VuVanHuy <huyvv10@fpt.edu.vn>
2026-09-24 11:01:07 +0700

    commit

--- SL05_GCD.c
+++ SL05_GCD.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 int main(){
-	int a, b;
+	int a, b, x, y;
 	scanf("%d%d", &a, &b);
-	
+	x = a; y = b;
 	while (a!=b){
 		if (a>b)
 			a=a-b;
@@ -11,5 +11,10 @@ int main(){
 			b=b-a;	
 	}
 	printf("Great common devisor is: %d", a);
+	int big, small;
+	big = x > y ? x : y;
+	small = x > y ? y : x;
+	printf("\nLeast common multiply of %d and %d is: %d", x, y, big/a*small);
+
 	return 0;
 }
---
+++ SL06_Triangle.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+//Chuong trinh tao tam gia vuong can bang dau *
+
+int main() {
+	int i, j, n;
+
+	scanf("%d", &n);
+	for (i=0; i<n; i++) {
+		for (j = 0; j <= i; j++) {
+			printf("* ");
+		}
+		printf("\n");
+	}
+
+	printf("\n");
+	for (i=n-1; i>=0; i--) {
+		for (j=0; j<=i; j++) {
+			printf("* ");
+		}
+		printf("\n");
+	}
+	return 0;
+}
